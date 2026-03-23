26b6610b8c9fa090adbaabbb1166d9569335d9f0
Negar Erfanian <n.erfanian93@gmail.com>
2026-03-23 14:35:17 -0500

    adding model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformationConfig, DataTransformation
-
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -44,4 +44,6 @@ if __name__ == '__main__':
     obj = DataIngestion()
     train_data, test_data = obj.initate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
